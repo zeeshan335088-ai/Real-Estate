c63f68f145b5c0f2e3122fa87e483b67cf8cf663
Zeeshan <zeeshan335088@gmail.com>
2026-03-06 15:48:37 +0500

    complete sign up page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+      '/api':{
+       target: 'http://localhost:3000',
+       secure:false,
+      },
+    },  
+  },
   plugins: [
     tailwindcss(),
     react()],
