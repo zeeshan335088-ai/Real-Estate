22a6bf6bfaa8283737484192f20ab2c10e2b6ad6
Zeeshan <zeeshan335088@gmail.com>
2026-03-10 14:02:21 +0500

    complete show user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -16,6 +16,8 @@ export default function Profile() {
   const [uploading, setUploading] = useState(false);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
+  const [showListingsError, setShowListingsError] =useState(false);
+  const [userListings, setUserListings] = useState({})
   const dispatch = useDispatch();
   
 
@@ -121,6 +123,22 @@ try {
    dispatch(signOutUserFailure(data.message))
 }
 }
+
+const handleShowListings = async () =>{
+  try{
+    setShowListingsError(false);
+  const res = await fetch(`/api/user/listings/${currentUser._id}`);
+  const data = await res.json();
+  if(data.success === false){
+    setShowListingsError(true);
+    return;
+  }
+
+  setUserListings(data);
+  }catch (error) {
+     setShowListingsError(true);
+  }
+};
   return (
     <div className="p-3 max-w-lg mx-auto">
 
@@ -201,6 +219,23 @@ try {
         <span  className="text-red-700 cursor-pointer" onClick={handleSignOut}>Sign out</span>
       </div>
       <p className="text-red-700 mt-5">{error ? error : ''}</p>
+      <button onClick={handleShowListings} className="text-green-700 w-full">Show Listings</button>
+   <p className="text-red-700 mt-5">
+    {showListingsError ? 'Error showing listings' : ''}
+   </p>
+   {userListings && userListings.length > 0 &&
+    userListings.map((listing) =><div key={listing._id} className="border rounded-lg p-3 flex justify-between items-center gap-4">
+ <Link to={`/listing/${listing._id}`}>
+ <img src={listing.imageUrls[0]} alt="listing cover" className="h-16 w-16 object-contain" />
+ </Link>
+<Link className="text-slate-700 font-semibold hover: underline truncate flex-1" to={`/listing/${listing._id}`}>
+<p >{listing.name}</p>
+</Link>
+<div className="flex flex-col items-center">
+<button className="text-red-700 uppercase">Delete</button>
+<button className="text-green-700 uppercase">Edit</button>
+</div>
+</div>)}
     </div>
   );
 }
