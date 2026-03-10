87f9082853bfd93c17ec12c8bcf087878b9c997f
Zeeshan <zeeshan335088@gmail.com>
2026-03-10 14:38:55 +0500

    complete delete user listing

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -139,6 +139,23 @@ const handleShowListings = async () =>{
      setShowListingsError(true);
   }
 };
+
+const handleListingDelete = async (listingId) =>{
+try {
+  const res = await fetch(`/api/listing/delete/${listingId}`, {
+    method: 'DELETE',
+});
+const data = await res.json();
+if(data.success === false){
+  console.log(data.message);
+  return;
+}
+
+setUserListings((prev) => prev.filter((listing) => listing._id !==listingId));
+} catch (error) {
+  console.log(error.message);
+}
+}
   return (
     <div className="p-3 max-w-lg mx-auto">
 
@@ -232,7 +249,7 @@ const handleShowListings = async () =>{
 <p >{listing.name}</p>
 </Link>
 <div className="flex flex-col items-center">
-<button className="text-red-700 uppercase">Delete</button>
+<button onClick={()=>handleListingDelete(listing._id)} className="text-red-700 uppercase">Delete</button>
 <button className="text-green-700 uppercase">Edit</button>
 </div>
 </div>)}
