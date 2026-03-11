3a8ecb17915351e8b9ec92cdf5bfd4694e0868b3
Zeeshan <zeeshan335088@gmail.com>
2026-03-11 13:29:24 +0500

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -26,4 +26,31 @@ return next(errorHandler(404, 'Listing not found!'))
    } catch (error) {
     next(error)  
    }
-}
+};
+
+
+export const updateListing = async (req, res, next) => {
+  try {
+
+    const listing = await Listing.findById(req.params.id);
+
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found!"));
+    }
+
+    if (req.user.id !== listing.userRef) {
+      return next(errorHandler(401, "You can only update your own listings!"));
+    }
+
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.status(200).json(updatedListing);
+
+  } catch (error) {
+    next(error);
+  }
+};
